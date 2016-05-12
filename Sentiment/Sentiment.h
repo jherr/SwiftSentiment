2a552920ce33c91c832720b08f613b2f06c25023
Jack Herrington <jack_d_herrington@pobox.com>
2016-05-11 20:13:41 -0700

    Initial Commit

---
+++ Sentiment/Sentiment.h
@@ -0,0 +1,19 @@
+//
+//  Sentiment.h
+//  Sentiment
+//
+//  Created by Jack Herrington on 5/11/16.
+//  Copyright © 2016 Jack Herrington. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Sentiment.
+FOUNDATION_EXPORT double SentimentVersionNumber;
+
+//! Project version string for Sentiment.
+FOUNDATION_EXPORT const unsigned char SentimentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sentiment/PublicHeader.h>
+
+
