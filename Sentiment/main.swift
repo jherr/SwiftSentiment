d6953fe6ab99316286f85dc4c126bb60992a7def
Jack Herrington <jherrington@walmartlabs.com>
2016-05-13 08:01:27 -0700

    First version

---
+++ Sentiment/main.swift
@@ -0,0 +1,42 @@
+//
+//  Sentiment.swift
+//  Sentiment
+//
+//  Created by Jack Herrington on 5/11/16.
+//  Copyright © 2016 Jack Herrington. All rights reserved.
+//
+
+import Foundation
+
+public struct SentimentResult {
+    var matches = 0;
+    var total = 0;
+}
+
+public class Sentiment {
+    public class func analyze(input: String) -> SentimentResult {
+        var afinn = AfinnData().getAfinn()
+        var value:SentimentResult = SentimentResult()
+        do {
+            let regex:NSRegularExpression = try NSRegularExpression(pattern: "\\W",
+                options: NSRegularExpressionOptions.CaseInsensitive);
+
+            let cleaned = regex.stringByReplacingMatchesInString(input,
+                options: NSMatchingOptions.WithoutAnchoringBounds,
+                range: NSMakeRange(0, input.characters.count),
+                withTemplate: " ");
+            
+            let words = cleaned.componentsSeparatedByString(" ");
+            
+            for word in words {
+                if ((afinn[word]) != nil) {
+                    value.total += afinn[word]!;
+                    value.matches += 1
+                }
+            }
+            print(value);
+        } catch {
+        }
+        return value
+    }
+}
---
+++ SentimentPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import Sentiment
+
+var str = "Hello, playground"
+
+Sentiment.analyze(str)
